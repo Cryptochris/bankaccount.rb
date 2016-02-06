0a20b5e0d0668d3da4ebd904fdf3334c29c0ea9e
Victor Goff <keeperotphones@gmail.com>
2016-02-06 16:47:45 -0500

    Fun with HereDocs
    Using Ruby 2.3 for the heredoc alignment, and remove the repetitive puts

--- bankaccount.rb
+++ bankaccount.rb
@@ -2,10 +2,10 @@
 class BankAccount
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, opening_balance = 0)
     @name = name
     @transactions = []
-    add_transaction("Beginning Balance", 0)
+    add_transaction('Beginning Balance', opening_balance)
   end
 
   def credit(description, amount)
@@ -27,30 +27,38 @@ class BankAccount
   end
 
   def to_s
-    my_string = "Name: %s, Balance: %0.2f"
-    sprintf(my_string, name, balance)
+    my_string = 'Name: %s, Balance: %0.2f'
+    format(my_string, name, balance)
   end
 
   def print_register
-    puts "#{name}'s Bank Account"
-    puts "-" * 40
+    # rubocop:disable Style/SpaceInsideStringInterpolation
+    puts(<<~EOS
 
-    puts "Description".ljust(30) + "Amount".rjust(10)
-    @transactions.each do |transaction|
-      puts transaction[:description].ljust(30) + sprintf("%0.2f", transaction[:amount]).rjust(10)
-    end
+    #{name}'s Bank Account
+    #{border = '-' * 40}
 
-    puts "-" * 40
-    puts "Balance:".ljust(30) + sprintf("%0.2f", balance).rjust(10)
-    puts "-" * 40
+    #{'Description'.ljust(30) + 'Amount'.rjust(10)}
+    #{
+    @transactions.map do |transaction|
+      transaction[:description].ljust(30) + format('%0.2f', transaction[:amount]).rjust(10)
+    end.join("\n")
+    }
+
+    #{border}
+    #{'Balance:'.ljust(30) + format('%0.2f', balance).rjust(10)}
+    #{border}
+    EOS
+        )
   end
-
 end
 
-bank_account = BankAccount.new("Jason")
-bank_account.credit("Paycheck", 100)
-bank_account.debit("Groceries", 40)
-bank_account.debit("Gas", 10.51)
-puts bank_account
-puts "Register:"
-bank_account.print_register
+if __FILE__ == $PROGRAM_NAME
+  bank_account = BankAccount.new('Jason', 250.00)
+  bank_account.credit('Paycheck', 100)
+  bank_account.debit('Groceries', 40)
+  bank_account.debit('Gas', 10.51)
+  puts bank_account
+  puts 'Register:'
+  bank_account.print_register
+end
